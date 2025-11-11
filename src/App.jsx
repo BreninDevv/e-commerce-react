0768c667832576487a4d8ea30ecf648839e8bd70
Breno Henrique <brenoeevil@gmail.com>
2025-11-11 16:09:30 -0300

    feat: menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import menuIcon from "./assets/icon-menu.svg";
+import logo from "./assets/logo.svg";
+import cart from "./assets/icon-cart.svg";
+import loginImage from "./assets/image-avatar.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <header>
+        <nav>
+          <div className="menu">
+            <a href="#">
+              <img src={menuIcon} alt="icon-menu" />
+            </a>
+            <a href="#" className="logo">
+              <img src={logo} alt="logo" />
+            </a>
 
-export default App
+            <a href="#">
+              <img src={cart} alt="cart" />
+            </a>
+            <a href="#">
+              <img src={loginImage} alt="login image" width={25} />
+            </a>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+export default App;
