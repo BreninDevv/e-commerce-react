956c0c63f99b7838b3b56a31c4643486609731c4
Breno Henrique <brenoeevil@gmail.com>
2025-11-14 00:51:23 -0300

    feat: interatividade dos botões do cart e dos botões para alterar a imagem

--- src/components/main components/cart.jsx
+++ src/components/main components/cart.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import IconMinus from "../../assets/icon-minus.svg";
 import IconPlus from "../../assets/icon-plus.svg";
 import IconCart from "../../assets/icon-cart.svg";
 
 const Cart = () => {
+  const [count, setCount] = useState(0);
+
+  const numberAdd = () => {
+    if (count === 20) {
+      setCount(count);
+    } else {
+      setCount(count + 1);
+    }
+  };
+  const numberRemove = () => {
+    if (count === 0) {
+      setCount(count);
+    } else {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <>
       <div className="cartContainer">
         <div className="cartBtns">
-          <button>
+          <button onClick={() => numberRemove()}>
             <img src={IconMinus} alt="icon-minus" />
           </button>
-          <span>0</span>
-          <button>
+          <span>{count}</span>
+          <button onClick={() => numberAdd()}>
             <img src={IconPlus} alt="icon-plus" />
           </button>
         </div>
--- src/components/main components/productImage.jsx
+++ src/components/main components/productImage.jsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import imageMain from "../../assets/image-product-1.jpg";
 import iconPrevious from "../../assets/icon-previous.svg";
 import iconNext from "../../assets/icon-next.svg";
+import image2 from "../../assets/image-product-2.jpg";
+import image3 from "../../assets/image-product-3.jpg";
+import image4 from "../../assets/image-product-4.jpg";
 
 const ProductImage = () => {
+  const [image, setImage] = useState(imageMain);
+
+  const nextImage = () => {
+    if (image === imageMain) {
+      setImage(image2);
+    } else if (image === image2) {
+      setImage(image3);
+    } else if (image === image3) {
+      setImage(image4);
+    }
+  };
+
+  const previousImage = () => {
+    if (image === image4) {
+      setImage(image3);
+    } else if (image === image3) {
+      setImage(image2);
+    } else if (image === image2) {
+      setImage(imageMain);
+    }
+  };
+
+  useEffect(() => {
+    if (image === imageMain) {
+      document.getElementById("previous").classList.add("on");
+    } else if (image === image4) {
+      document.getElementById("next").classList.add("on");
+    } else {
+      document.getElementById("previous").classList.remove("on");
+      document.getElementById("next").classList.remove("on");
+    }
+  }, [image]);
+
   return (
     <>
       <div className="images-main">
-        <img className="images" src={imageMain} alt="image-main" />
+        <img className="images" src={image} alt="image-main" />
 
-        <div className="btnPrevious">
+        <div
+          onClick={() => previousImage()}
+          id="previous"
+          className="btnPrevious"
+        >
           <img src={iconPrevious} alt="icon-previous" />
         </div>
-        <div className="btnNext">
+        <div onClick={() => nextImage()} id="next" className="btnNext">
           <img src={iconNext} alt="" />
         </div>
       </div>
